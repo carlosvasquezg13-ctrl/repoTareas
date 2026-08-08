d5db07724722ddca6d736af7a56aa265972973c3
carlosvasquezg13-ctrl <carlosvasquezg13@gmail.com>
2026-08-08 15:51:28 -0600

    Update eje2.js

--- eje2.js
+++ eje2.js
@@ -7,7 +7,7 @@ rl.question("Ingrese la base: ",function(base){
     rl.question("Ingrese la altura. ",function(altura){
         base=Number(base)
         altura=Number(altura)
-        const area=(base*altura)/2
+        const area=(base*altura)
         console.log(`El area total es: ${area}` )
         rl.close()
     })
